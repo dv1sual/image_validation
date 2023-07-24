a5f584359ed561e25d7923f0976ab192835963c1
dv1sual <53606943+dv1sual@users.noreply.github.com>
2023-07-24 21:22:50 +0100

    Added ITU-R_BT.709 The RGB values are not 32bit floats

--- img_val.py
+++ img_val.py
@@ -4,41 +4,22 @@ from PIL import Image  # For image processing
 import OpenEXR  # For opening EXR files
 import Imath  # For handling EXR files
 import os  # For handling file paths
+import logging
 from utils.logger_config import configure_logger  # For logging configuration
 import json  # For handling JSON files
+
+
+logger = logging.getLogger()
+
 
 # Load color chart values from a JSON file
 with open('color_charts_values/color_chart_values.json', 'r') as file:
     color_charts = json.load(file)
 
-rgb_color_chart_values = color_charts['RGB_color_chart_values']
 aces2065_1_color_chart_values = color_charts['aces2065_1_color_chart_values']
 itu_r_bt_2020_color_chart_values = color_charts['itu_r_bt_2020_color_chart_values']
-
-
-def load_image_rgb(image_path):
-    """
-    Load an RGB image using PIL and print info messages.
-    """
-    global logger
-    logger = configure_logger('rgb')
-    if not os.path.isfile(image_path):
-        raise FileNotFoundError(f"The image file {image_path} does not exist.")
-
-    filename = os.path.basename(image_path)
-    logger.info("RGB Color Check")
-    logger.info("Loading image, please wait...")
-
-    try:
-        image = Image.open(image_path)
-        image = np.array(image)  # convert PIL Image to numpy array
-    except Exception as image_load_exception:
-        logger.error(f"Failed to load image: {image_load_exception}")
-        raise
-
-    logger.info(f"Successfully loaded {filename}")
-    logger.info(f"Color checking starting...")
-    return image
+itu_r_bt_709_color_chart_values = color_charts['itu_r_bt_709_color_chart_values']
+rgb_color_chart_values = color_charts['RGB_color_chart_values']
 
 
 def load_image_aces(image_path):
@@ -105,7 +86,101 @@ def load_image_itu2020(image_path):
     return image
 
 
-def validate_color_chart(image, chart_values, tolerance=0.05):
+def load_image_itu709(image_path):
+    """Load an ITU-R BT.709 image file and return it as a numpy array."""
+    global logger
+    logger = configure_logger('itu709')
+    if not os.path.isfile(image_path):
+        raise FileNotFoundError(f"The image file {image_path} does not exist.")
+
+    filename = os.path.basename(image_path)
+    logger.info("ITU-R BT.709 Color Check")
+    logger.info("Loading image...")
+
+    if image_path.lower().endswith(".exr"):
+        exr_file = OpenEXR.InputFile(image_path)
+        dw = exr_file.header()['dataWindow']
+        size = (dw.max.x - dw.min.x + 1, dw.max.y - dw.min.y + 1)
+
+        pt = Imath.PixelType(Imath.PixelType.FLOAT)
+        r_str, g_str, b_str = exr_file.channel('R', pt), exr_file.channel('G', pt), exr_file.channel('B', pt)
+
+        r_channel, g_channel, b_channel = [np.frombuffer(channel_str, dtype=np.float32).reshape((size[1], size[0])) for
+                                           channel_str in [r_str, g_str, b_str]]
+        image = np.dstack((r_channel, g_channel, b_channel))
+        logger.info(f"Successfully loaded {filename}")
+    else:
+        logger.error(f"Loading non-EXR file for ITU-R BT.709: {image_path}")
+        raise ValueError(f"Non-EXR file provided for ITU-R BT.709 color chart: {image_path}")
+
+    logger.info("Color checking starting...")
+    return image
+
+
+def load_image_rgb(image_path):
+    """
+    Load an RGB image using PIL and print info messages.
+    """
+    global logger
+    logger = configure_logger('rgb')
+    if not os.path.isfile(image_path):
+        raise FileNotFoundError(f"The image file {image_path} does not exist.")
+
+    filename = os.path.basename(image_path)
+    logger.info("RGB Color Check")
+    logger.info("Loading image, please wait...")
+
+    try:
+        image = Image.open(image_path)
+        image = np.array(image)  # convert PIL Image to numpy array
+        # Normalize the image array values to the range 0.0 - 1.0 if they aren't already
+        if np.max(image) > 1.0:
+            image = image / 255.0
+    except Exception as image_load_exception:
+        logger.error(f"Failed to load image: {image_load_exception}")
+        raise
+
+    logger.info(f"Successfully loaded {filename}")
+    logger.info(f"Color checking starting...")
+    return image
+
+
+def visualize_color_chart(image, chart_values, color_space, image_path):
+    """Visualize a color chart as an image."""
+    canvas_height, canvas_width = 140 * len(chart_values) + 20, 500
+    canvas_vis = np.ones((canvas_height, canvas_width, 3), dtype=np.uint8) * 255
+
+    for i, value in enumerate(chart_values):
+        expected_rgb, actual_rgb = value["rgb"], image[value["y"], value["x"]]
+        top, bottom = 10 + i * 140, 10 + i * 140 + 120
+        expected_rgb_int, actual_rgb_int = [tuple([int(c * 255) for c in rgb][::-1]) for rgb in
+                                            [expected_rgb, actual_rgb]]
+
+        accuracy_percentage = value["accuracy"] * 100
+        border_color = (0, 255, 0) if accuracy_percentage >= 99.97 else (0, 0, 255)
+
+        cv2.rectangle(canvas_vis, (50, top), (150, bottom), border_color, 2)
+        cv2.rectangle(canvas_vis, (250, top), (350, bottom), border_color, 2)
+        cv2.rectangle(canvas_vis, (51, top + 1), (149, bottom - 1), expected_rgb_int, -1)
+        cv2.rectangle(canvas_vis, (251, top + 1), (349, bottom - 1), actual_rgb_int, -1)
+
+        cv2.putText(canvas_vis, value["name"], (10, top + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
+
+        for j, color_name in enumerate(['R', 'G', 'B']):
+            expected_text = f"{color_name}: {expected_rgb[j]:.3f}"
+            actual_text = f"{color_name}: {actual_rgb[j]:.3f}"
+            y_offset = top + 20 + j * 20  # Increment y position for each color component
+            cv2.putText(canvas_vis, expected_text, (155, y_offset), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1,
+                        cv2.LINE_AA)
+            cv2.putText(canvas_vis, actual_text, (355, y_offset), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1,
+                        cv2.LINE_AA)
+
+    output_filename = f"{os.path.splitext(os.path.basename(image_path))[0]}_{color_space}_result.png"
+    output_path = os.path.join('results', output_filename)
+    cv2.imwrite(output_path, canvas_vis)
+
+
+def validate_color_chart(image, chart_values, color_space, image_path, tolerance=0.05):
     """Check the color accuracy of an image based on a color chart."""
     passed, failed, total_accuracy = 0, 0, 0
 
@@ -143,17 +218,27 @@ def validate_color_chart(image, chart_values, tolerance=0.05):
     logger.info(f"Failed checks: {failed}")
     logger.info(f"Overall accuracy: {overall_accuracy:.2%}")
 
+    # Create a visualisation for the current color space
+    output_path = f'result_{color_space}.png'  # filename based on color space
+    visualize_color_chart(image, chart_values, color_space, image_path)
+
     return passed, failed, overall_accuracy, chart_values
 
 
 def configure_loggers(color_space):
     # Initialize logger for the specified color space
     loggers = {
-        'rgb': configure_logger('rgb', 'rgb.log'),
-        'aces2065_1': configure_logger('aces2065_1', 'aces2065_1.log'),
-        'itu2020': configure_logger('itu2020', 'itu2020.log')
+        'rgb': configure_logger('rgb'),
+        'aces2065_1': configure_logger('aces2065_1'),
+        'itu2020': configure_logger('itu2020'),
+        'itu709': configure_logger('itu709')
     }
     return loggers[color_space]
+
+
+def visualize_validation(image, chart_values):
+    output_path = "results/"
+    visualize_color_chart(image, chart_values, output_path)
 
 
 def validate_single_image(image_path, color_space):
@@ -170,10 +255,13 @@ def validate_single_image(image_path, color_space):
     elif color_space == "itu2020":
         image = load_image_itu2020(image_path)
         color_chart_values = itu_r_bt_2020_color_chart_values
+    elif color_space == "itu709":
+        image = load_image_itu709(image_path)
+        color_chart_values = itu_r_bt_709_color_chart_values
     else:
         raise ValueError(f"Unknown color space: {color_space}")
 
-    passed, failed, accuracy, _ = validate_color_chart(image, color_chart_values)
+    passed, failed, accuracy, chart_values = validate_color_chart(image, color_chart_values, color_space, image_path)
     if passed == len(color_chart_values):
         logger.info("Image passed the color check.")
     else:
@@ -185,6 +273,7 @@ def main():
     image_color_space_pairs = [
         ('image_charts/ACES2065_1_1920_1080.exr', 'aces2065_1'),
         ('image_charts/ITU-R_BT.2020.exr', 'itu2020'),
+        ('image_charts/ITU-R_BT.709.exr', 'itu709'),
         ('image_charts/RGB_1920_1080.tif', 'rgb')
     ]
 
